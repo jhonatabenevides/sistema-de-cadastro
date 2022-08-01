8b71b26f2156b4f8298d5db06ca12e29a95bbf88
Jbenevides <jhonatabenevides1995@gmail.com>
2022-07-31 23:25:17 -0300

    Atualizando meu sistema de cadastro

--- app.js
+++ app.js
@@ -2,5 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log ("Aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("O Jhonata fez esse commit sem você ver na aula!")
-console.log("Enviando para o GitHub")
-consele.log("praticando.")
+console.log("Sistema de atualização de cadastro do usuario")
