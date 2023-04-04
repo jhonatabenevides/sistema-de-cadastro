0cad5ef4e447cfced2a26e4bf889ffbe8e6f8412
Jhonata Benevides <91574341+jhonatabenevides@users.noreply.github.com>
2023-04-03 23:56:23 -0300

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
-console.log ("Aplicação está se conectando ao banco de dados")
-// mais um console log de teste:
-console.log("O Jhonata fez esse commit sem você ver na aula!")
-console.log("Sistema de atualização de cadastro do usuario")
